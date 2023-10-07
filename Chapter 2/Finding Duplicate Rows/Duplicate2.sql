1a45a237796a2eb4df7ff3ceab164953a8e8ef8a
Olesia Dolzhenkova <olesinionz@gmail.com>
2023-10-07 03:02:49 +0000

    Add new column name to check the data

--- Chapter 2/Finding Duplicate Rows/Duplicate2.sql
+++ Chapter 2/Finding Duplicate Rows/Duplicate2.sql
@@ -1,4 +1,4 @@
-SELECT FirstName, LastName, COUNT(1)
+SELECT FirstName, LastName, Email, COUNT(1)
 FROM Customer
-GROUP BY FirstName, LastName
+GROUP BY FirstName, LastName, Email
 HAVING COUNT(1) > 1
