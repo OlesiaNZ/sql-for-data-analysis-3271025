47e1d8c3bc0b1906cf9fd0a10e224e1306db648a
Olesia Dolzhenkova <olesinionz@gmail.com>
2023-10-07 04:08:04 +0000

    add with separating data

--- Chapter 2/Finding Duplicate Rows/Duplicate2.sql
+++ Chapter 2/Finding Duplicate Rows/Duplicate2.sql
@@ -1,4 +1,4 @@
 SELECT FirstName, LastName, Email, COUNT(1)
 FROM Customer
 GROUP BY FirstName, LastName, Email
-HAVING COUNT(1) > 1
+HAVING COUNT(1) > 1;
---
+++ Chapter 3/WorkingWithDates/SeparateDateByMyself.sql
@@ -0,0 +1,4 @@
+SELECT Year(CreationDate) as YearOrder,
+  Month(CreationDate) as MonthOrder,
+  Day(CreationDate) as DayOrder
+From Orders
